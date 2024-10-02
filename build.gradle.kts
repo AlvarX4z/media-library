0a2c23f61928d76b1c6051ca119bb93b5ce48207
Álvaro de Francisco <alfrasan@gmail.com>
2024-10-02 16:32:03 +0200

    Add testFixtures directory

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 plugins {
+    id("java-test-fixtures")
     id("org.jlleitschuh.gradle.ktlint") version "12.1.1"
     id("io.spring.dependency-management") version "1.1.6"
     id("org.springframework.boot") version "3.3.4"
