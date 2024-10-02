763326c7f70543d963cabb0a42e39a86fe1dddf5
Álvaro de Francisco <alfrasan@gmail.com>
2024-10-02 20:22:23 +0200

    Add BirthDate

--- src/testFixtures/kotlin/alvarx4z/medialibrary/SharedHelper.kt
+++ src/testFixtures/kotlin/alvarx4z/medialibrary/SharedHelper.kt
@@ -1,12 +1,18 @@
 package alvarx4z.medialibrary
 
 import alvarx4z.medialibrary.domain.shared.invariant.NotEmptyString
+import alvarx4z.medialibrary.domain.shared.professional.BirthDate
 import alvarx4z.medialibrary.domain.shared.professional.Name
+import java.time.LocalDate
+import java.time.Month
 
 object SharedHelper {
     private const val FIRST_NAME_VALUE = "Michael Peter"
     private const val LAST_NAME_VALUE = "Balzary"
     private const val PSEUDONYM_VALUE = "Flea"
+
+    private const val YEAR_VALUE = 1962
+    private const val DAY_VALUE = 16
 
     private fun firstName(firstName: String = FIRST_NAME_VALUE) = NotEmptyString(firstName)
 
@@ -23,4 +29,10 @@ object SharedHelper {
         lastName = lastName,
         pseudonym = pseudonym,
     )
+
+    fun publicationDate(
+        year: Int = YEAR_VALUE,
+        month: Month = Month.OCTOBER,
+        day: Int = DAY_VALUE,
+    ) = BirthDate(LocalDate.of(year, month, day))
 }
