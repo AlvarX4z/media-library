54ed5a1277dbb394eb5cab295f1f02de9ca075a9
Álvaro de Francisco <alfrasan@gmail.com>
2024-10-02 16:55:01 +0200

    Expand error message

--- src/main/kotlin/alvarx4z/medialibrary/domain/shared/invariant/PastDate.kt
+++ src/main/kotlin/alvarx4z/medialibrary/domain/shared/invariant/PastDate.kt
@@ -5,7 +5,7 @@ import java.time.LocalDate
 open class PastDate(date: LocalDate) {
     init {
         require(date.isBefore(LocalDate.now())) {
-            "The date cannot be in the future"
+            "The provided date cannot be set in the future"
         }
     }
 }
