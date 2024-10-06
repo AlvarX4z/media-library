8dc58544386d1a16283275211c49a36a4900be35
Álvaro de Francisco <alfrasan@gmail.com>
2024-10-06 12:10:02 +0200

    Add Location

---
+++ src/test/kotlin/alvarx4z/medialibrary/domain/shared/professional/LocationTest.kt
@@ -0,0 +1,18 @@
+package alvarx4z.medialibrary.domain.shared.professional
+
+import alvarx4z.medialibrary.SharedHelper
+import alvarx4z.medialibrary.domain.shared.Country
+import alvarx4z.medialibrary.domain.shared.invariant.NotEmptyString
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+internal class LocationTest {
+  @Test
+  fun `should instantiate a valid Location`() {
+    val location = SharedHelper.location()
+
+    assertThat(location).isInstanceOf(Location::class.java)
+    assertThat(location.city).isInstanceOf(NotEmptyString::class.java)
+    assertThat(location.country).isInstanceOf(Country::class.java)
+  }
+}
