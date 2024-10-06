27d93c59f16ba2ec14160cb74480662d6d030dcd
Álvaro de Francisco <alfrasan@gmail.com>
2024-10-06 12:11:03 +0200

    Add Professional

--- src/testFixtures/kotlin/alvarx4z/medialibrary/SharedHelper.kt
+++ src/testFixtures/kotlin/alvarx4z/medialibrary/SharedHelper.kt
@@ -1,8 +1,8 @@
 package alvarx4z.medialibrary
 
+import alvarx4z.medialibrary.domain.shared.Country
 import alvarx4z.medialibrary.domain.shared.invariant.NotEmptyString
-import alvarx4z.medialibrary.domain.shared.professional.Name
-import alvarx4z.medialibrary.domain.shared.professional.birth.Date
+import alvarx4z.medialibrary.domain.shared.professional.*
 import java.time.LocalDate
 import java.time.Month
 
@@ -13,8 +13,12 @@ object SharedHelper {
   private const val SECOND_PSEUDONYM_VALUE = "John Swithen"
   private const val THIRD_PSEUDONYM_VALUE = "Beryl Evans"
 
-  private const val YEAR_VALUE = 1947
-  private const val DAY_VALUE = 21
+  private const val BIRTH_YEAR_VALUE = 1947
+  private const val BIRTH_DAY_VALUE = 21
+  private const val DEATH_YEAR_VALUE = 2024
+  private const val DEATH_DAY_VALUE = 17
+
+  private const val CITY_VALUE = "Portland"
 
   private fun firstName(firstName: String = FIRST_NAME_VALUE) = NotEmptyString(firstName)
 
@@ -33,6 +37,33 @@ object SharedHelper {
     pseudonyms: List<NotEmptyString>? = pseudonym(),
   ) = Name(firstName = firstName, lastName = lastName, pseudonyms = pseudonyms)
 
-  fun birthDate(year: Int = YEAR_VALUE, month: Month = Month.SEPTEMBER, day: Int = DAY_VALUE) =
-    Date(LocalDate.of(year, month, day))
+  fun birthDate(
+    year: Int = BIRTH_YEAR_VALUE,
+    month: Month = Month.SEPTEMBER,
+    day: Int = BIRTH_DAY_VALUE,
+  ) = Date(LocalDate.of(year, month, day))
+
+  private fun city(value: String = CITY_VALUE) = NotEmptyString(value)
+
+  fun location(city: NotEmptyString = city(), country: Country = Country.UNITED_STATES) =
+    Location(city = city, country = country)
+
+  fun birth(date: Date = birthDate(), location: Location = location()) =
+    Birth(date = date, location = location)
+
+  private fun deathDate(
+    year: Int = DEATH_YEAR_VALUE,
+    month: Month = Month.JULY,
+    day: Int = DEATH_DAY_VALUE,
+  ) = Date(LocalDate.of(year, month, day))
+
+  fun death(date: Date = deathDate(), location: Location = location()) =
+    Death(date = date, location = location)
+
+  fun professional(
+    name: Name = name(),
+    profession: List<Profession> = listOf(Profession.WRITER),
+    birth: Birth = birth(),
+    death: Death? = death(),
+  ) = Professional(name = name, profession = profession, birth = birth, death = death)
 }
