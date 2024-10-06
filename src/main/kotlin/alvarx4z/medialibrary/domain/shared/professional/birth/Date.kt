7b73cef796a11bb7990d81572ac4045f03334ac7
Álvaro de Francisco <alfrasan@gmail.com>
2024-10-06 10:12:23 +0200

    Refactor BirthDate to Date

---
+++ src/main/kotlin/alvarx4z/medialibrary/domain/shared/professional/birth/Date.kt
@@ -0,0 +1,6 @@
+package alvarx4z.medialibrary.domain.shared.professional.birth
+
+import alvarx4z.medialibrary.domain.shared.invariant.PastDate
+import java.time.LocalDate
+
+class Date(date: LocalDate) : PastDate(date)
