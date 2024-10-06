7ea632ac690ad46511417b6c74bb6a97b54bdced
Álvaro de Francisco <alfrasan@gmail.com>
2024-10-06 12:09:53 +0200

    Add Country

---
+++ src/test/kotlin/alvarx4z/medialibrary/domain/shared/CountryTest.kt
@@ -0,0 +1,17 @@
+package alvarx4z.medialibrary.domain.shared
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+internal class CountryTest {
+  @Test
+  fun `should return the correct enum constant from value`() {
+    assertThat(Country.UNITED_STATES).isEqualTo(Country.fromValue("United States"))
+  }
+
+  @Test
+  fun `should throw IllegalArgumentException when creating a new Country from invalid value`() {
+    assertThrows<IllegalArgumentException> { Country.valueOf("Fake") }
+  }
+}
